25a38691583f1bc4d8a6651a64ac6283e128936b
fvucemilo <fvucemilo@tvz.hr>
2021-11-13 01:28:52 +0100

    MODEL_URI fix

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import json
 import requests
 
 SIZE=(200, 200)
-MODEL_URI=os.environ.get('MODEL_URI')
+MODEL_URI='https://wine-model-tfs.herokuapp.com/v1/models/wine_model:predict'
 CLASSES = ['b1', 'b10', 'b11', 'b12', 'b13', 'b14', 'b15', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9']
 
 def get_prediction(image_path):
